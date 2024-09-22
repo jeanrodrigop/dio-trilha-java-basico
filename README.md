4d01ae5886dcbbd1895d866ddb98be262eece845
jeanrodrigop <jeanrodrigo_p@outlook.com>
2024-09-22 22:56:19 +0000

    Modulo - Anatomia das Classes

---
+++ README.md
@@ -0,0 +1,2 @@
+# dio-trilha-java-basico
+Repositório para exercícios de Java referentes ao programa Claro - Java com Spring Boot da Dio.
---
+++ anatomia-classes/anatomia-classes/src/edu/jean/segundasemana/MinhaClasse.java
@@ -0,0 +1,14 @@
+package edu.jean.segundasemana;
+public class MinhaClasse {
+    public static void main (String [] args) {
+        String primeiroNome = "Jean";
+        String segundoNome = "Rodrigo";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
