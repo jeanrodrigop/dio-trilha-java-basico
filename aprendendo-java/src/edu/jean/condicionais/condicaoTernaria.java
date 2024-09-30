567d17397a939849118aa8ff4e28e55ed9bc58fc
jeanrodrigop <jeanrodrigo_p@outlook.com>
2024-09-30 22:51:22 +0000

    Condicionais em Java

---
+++ aprendendo-java/src/edu/jean/condicionais/condicaoTernaria.java
@@ -0,0 +1,10 @@
+package edu.jean.condicionais;
+
+public class condicaoTernaria {
+    public static void main(String[] args) {
+        int nota = 6;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota <7 ? "Recuperação" : "Reprovado";
+
+        System.out.println(resultado);
+    }
+}
---
+++ aprendendo-java/src/edu/jean/condicionais/condicionalEncadeada.java
@@ -0,0 +1,14 @@
+package edu.jean.condicionais;
+
+public class condicionalEncadeada {
+    public static void main(String[] args) {
+        int nota = 8;
+
+        if(nota >= 7)
+            System.out.println("Aprovado");
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova de recuperação");            
+        else
+            System.out.println("Reprovado");
+    }
+}
