264a873366c8f710e18af76391be7581d2401534
jeanrodrigop <jeanrodrigo_p@outlook.com>
2024-09-30 23:28:45 +0000

    Repeticoes em Java

---
+++ aprendendo-java/src/edu/jean/repeticoes/doWhile.java
@@ -0,0 +1,20 @@
+package edu.jean.repeticoes;
+
+import java.util.Random;
+
+public class doWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+        do {
+            System.out.println("Telefone tocando");
+        } while (tocando());
+
+        System.out.println("Alo!");
+    }
+
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu? " + atendeu);
+        return ! atendeu;
+    }
+}
---
+++ aprendendo-java/src/edu/jean/repeticoes/forArrays.java
@@ -0,0 +1,22 @@
+package edu.jean.repeticoes;
+
+public class forArrays {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"}
+
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x= " + x + " e " + alunos[x]);
+        }
+    }
+}
+
+public class forEach {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"}
+    
+        for (String aluno : alunos) {
+            System.out.println("O nome aluno e: " + aluno);
+        }
+    }
+
+}
