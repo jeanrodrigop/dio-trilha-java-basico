063fa3d83cf171292db2a917e6cb32ab99d40bfc
jeanrodrigop <jeanrodrigo_p@outlook.com>
2024-10-07 02:47:32 +0000

    Java e tratamento de excecoes

--- aprendendo-java/src/edu/jean/condicionais/condicionalSimples.java
+++ aprendendo-java/src/edu/jean/condicionais/condicionalSimples.java
@@ -3,7 +3,7 @@ package edu.jean.condicionais;
 class condicionalSimples {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0
+        double valorSolicitado = 17.0;
 
         if(valorSolicitado < saldo)
             saldo = saldo - valorSolicitado;
--- aprendendo-java/src/edu/jean/repeticoes/forEach.java
+++ aprendendo-java/src/edu/jean/repeticoes/forEach.java
@@ -2,7 +2,7 @@ package edu.jean.repeticoes;
 
 public class forArrays {
     public static void main(String[] args) {
-        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"}
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
 
         for (int x=0; x<alunos.length; x++) {
             System.out.println("O aluno no indice x= " + x + " e " + alunos[x]);
@@ -12,7 +12,7 @@ public class forArrays {
 
 public class forEach {
     public static void main(String[] args) {
-        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"}
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
     
         for (String aluno : alunos) {
             System.out.println("O nome aluno e: " + aluno);
